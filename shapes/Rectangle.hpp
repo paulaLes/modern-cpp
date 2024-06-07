a4d88e5f75827cde00b24041ac534f43f15e6ebf
paulaLes <paula.lesiczka@gmail.com>
2024-06-07 12:28:03 +0200

    Update Rectangle.hpp

--- shapes/Rectangle.hpp
+++ shapes/Rectangle.hpp
@@ -2,20 +2,20 @@
 
 #include "Shape.hpp"
 
-class Rectangle : public Shape {
+class Rectangle : public Shape
+{
 public:
+    Rectangle() = delete;
     Rectangle(double x, double y);
-    Rectangle(const Rectangle& other);
+    Rectangle(const Rectangle & other) = default;
 
-    double getArea() const;
-    double getPerimeter() const;
-    double getX() const;
+    double getArea() const noexcept override;
+    double getPerimeter() const override;
+    virtual double getX() const final;
     double getY() const;
-    void print() const;
+    void print() const override;
 
 private:
-    Rectangle();
-
     double x_;
     double y_;
 };
