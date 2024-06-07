2aa41f698d4d2bea1ebfeaa299d8872fbb2422bb
paulaLes <paula.lesiczka@gmail.com>
2024-06-07 11:33:24 +0200

    New function added

--- shapes/main.cpp
+++ shapes/main.cpp
@@ -12,23 +12,35 @@ using namespace std;
 
 typedef vector<shared_ptr<Shape>> Collection;
 
-bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second) {
-    if (first == NULL || second == NULL)
-        return false;
-    return (first->getArea() < second->getArea());
+
+template<class DerivedType, class... Arguments>
+std::shared_ptr<Shape> make_shape(Arguments&&... args) {
+    return std::make_shared<DerivedType>(std::forward<Arguments>(args)...);
 }
 
-bool perimeterBiggerThan20(shared_ptr<Shape> s) {
-    if (s)
-        return (s->getPerimeter() > 20);
-    return false;
+
+template<typename T, typename = std::enable_if_t<std::is_base_of<Shape, T>::value>>
+void addShape(std::shared_ptr<T> shape, std::vector<std::shared_ptr<Shape>>& collection) {
+    collection.push_back(shape);
 }
 
-bool areaLessThan10(shared_ptr<Shape> s) {
-    if (s)
-        return (s->getArea() < 10);
-    return false;
-}
+//bool sortByArea(shared_ptr<Shape> first, shared_ptr<Shape> second) {
+   // if (first == NULL || second == NULL)
+   //     return false;
+ //   return (first->getArea() < second->getArea());
+//}
+
+//bool perimeterBiggerThan20(shared_ptr<Shape> s) {
+//    if (s)
+//        return (s->getPerimeter() > 20);
+//    return false;
+//}
+
+//bool areaLessThan10(shared_ptr<Shape> s) {
+//    if (s)
+//        return (s->getArea() < 10);
+//    return false;
+//}
 
 void printCollectionElements(const Collection& collection) {
     for (Collection::const_iterator it = collection.begin(); it != collection.end(); ++it)
@@ -64,6 +76,19 @@ int main() {
     shapes.push_back(make_shared<Square>(3.0));
     shapes.push_back(make_shared<Circle>(4.0));
     printCollectionElements(shapes);
+
+    auto sortByArea =  {
+        return first != nullptr && second != nullptr && first->getArea() < second->getArea();
+    };
+
+    auto perimeterBiggerThan20 =  {
+        return s != nullptr && s->getPerimeter() > 20;
+    };
+
+    int x = 10;
+    auto areaLessThanX = x {
+        return s != nullptr && s->getArea() < x;
+    };
 
     cout << "Areas before sort: " << std::endl;
     printAreas(shapes);
